9c3e4e50565f2df4b9c3bed77e3d741d5a80017f
biniabera274 <biniabera274@gmail.com>
2021-08-07 16:14:13 +0300

    done chat app!!

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json({users:[{name:"Benjamin",age:24},{name:"Loza",age:21}]});
 });
 
 module.exports = router;
